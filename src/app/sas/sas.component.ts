600181f66f5be0e2c0d72261ee2c4d3d558df140
Sameer Kumar <kumar.sam1612@gmail.com>
2024-02-16 14:17:58 +0530

    checkpoint

--- src/app/sas/sas.component.ts
+++ src/app/sas/sas.component.ts
@@ -1,72 +1,37 @@
+import { Component } from '@angular/core';
 import { AzureBlobStorageService } from './../azure-blob-storage.service';
-import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sas',
   templateUrl: './sas.component.html',
   styleUrls: ['./sas.component.css'],
 })
-export class SasComponent implements OnInit {
-  // SAS (shared access signatures)
-
+export class SasComponent {
   res = {
-    'peter-545978099855900731.png':
-      'https://stgisappsdevwedigihub001.blob.core.windows.net/dh-temp-file-store/peter-545978099855900731.png?sv=2023-08-03&st=2024-02-15T14%3A42%3A02Z&se=2024-02-15T14%3A47%3A02Z&sr=b&sp=rw&sig=Pe8y52OXZwdBgjP17Jn0Cs9GzvxCJl6hJCdQgP7t4oY%3D',
+    'peter-546250436157227224.jpg':
+      'https://stgisappsdevwedigihub001.blob.core.windows.net/dh-temp-file-store/peter-546250436157227224.jpg?sv=2023-08-03&st=2024-02-16T08%3A44%3A12Z&se=2024-02-16T08%3A49%3A12Z&sr=b&sp=rw&sig=PbdvNu0i%2FLv3W5RGd%2Ba4KvpRZK8TZIdYK%2BgnoDIdUd4%3D',
   };
-
-  sas =
-    'sv=2023-08-03&st=2024-02-15T14%3A42%3A02Z&se=2024-02-15T14%3A47%3A02Z&sr=b&sp=rw&sig=Pe8y52OXZwdBgjP17Jn0Cs9GzvxCJl6hJCdQgP7t4oY%3D';
-
-  picturesList: string[] = [];
-  picturesDownloaded: string[] = [];
-
-  videosList: string[] = [];
-  videoDownloaded;
 
   constructor(private blobService: AzureBlobStorageService) {}
 
-  ngOnInit(): void {
-    this.reloadImages();
-  }
-
-  public setSas(event) {
-    this.sas = event.target.value;
-  }
-
   public imageSelected(file: File) {
-    this.blobService.uploadImage(this.sas, file, file.name, () => {
-      this.reloadImages();
-    });
+    this.blobService.uploadImage(
+      this.parseSasResponse(),
+      file,
+      file.name,
+      () => {}
+    );
   }
 
-  public deleteImage(name: string) {
-    this.blobService.deleteImage(this.sas, name, () => {
-      this.reloadImages();
-    });
-  }
-
-  public downloadImage(name: string) {
-    this.blobService.downloadImage(this.sas, name, (blob) => {
-      let url = window.URL.createObjectURL(blob);
-      window.open(url);
-    });
-  }
-
-  private reloadImages() {
-    this.blobService.listImages(this.sas).then((list) => {
-      this.picturesList = list;
-      const array = [];
-      this.picturesDownloaded = array;
-
-      for (let name of this.picturesList) {
-        this.blobService.downloadImage(this.sas, name, (blob) => {
-          var reader = new FileReader();
-          reader.readAsDataURL(blob);
-          reader.onloadend = function () {
-            array.push(reader.result as string);
-          };
-        });
-      }
-    });
+  parseSasResponse() {
+    return {
+      sas: Object.values(this.res)[0].split('?')[1],
+      filename: Object.keys(this.res)[0],
+      blobServiceUrl: Object.values(this.res)[0]
+        .split('/')
+        .slice(0, 3)
+        .join('/'),
+      containerName: Object.values(this.res)[0].split('/')[3],
+    };
   }
 }
